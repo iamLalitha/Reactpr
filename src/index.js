1cedefcbee8c2f15f0e7fea5a8dbe77c321f52b5
iamLalitha <lalithasans10@gmail.com>
2023-06-15 19:18:20 +0530

    exercise1

--- src/index.js
+++ src/index.js
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+// const root = ReactDOM.createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+//strict mode is used to identify any deprecated tags are there or not. it just to check.
+//the below line removes the strict mode. it is same as like above code lines from 6-10. 
+//the below code jus removes the stirct mode.
+ReactDOM.createRoot(document.getElementById('root')).render(<App/>);
